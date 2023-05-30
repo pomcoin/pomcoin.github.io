e5eb06fb493602fb910328bbc88c09dce9a648e3
pomcoin <pomcointoken@gmail.com>
2023-05-30 18:03:38 +0800

    Main Pages Created and Added to Website
    Pomcoin Crypto Token Website Release

---
+++ vue.js
@@ -0,0 +1,13 @@
+Vue.component('falling', {
+    template: "#falling",
+  })
+  
+  new Vue({
+    el: '#app',
+    data() {
+      return {
+      }
+    },
+    computed:{},
+    methods:{}
+  });
